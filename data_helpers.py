5c82f397db99a78431e839fcf784093ce1b4a488
cmwenliu <cmwenliu@gmail.com>
2018-03-12 16:25:29 -0500

    labeled data dir added

--- data_helpers.py
+++ data_helpers.py
@@ -45,7 +45,6 @@ def load_data_and_labels(data_dir):
     y = np.concatenate([positive_labels, negative_labels], 0)
     return [x_text, y]
 	'''
-    #data_sets = glob.glob("./data/*.txt")
     #store all of the class data in a list
     class_data = []
     label_list = []
@@ -103,6 +102,16 @@ def load_embedding_vectors(vocabulary, filename, vector_size):
             embedding_vectors[idx] = vector
     f.close()
     return embedding_vectors
+
+def train_word_emb(data_sets, dimension):
+    for data in data_sets:
+	head, filename = os.path.split(data)
+	filename += '.vec100'
+	filepath = os.path.join(head, filename)
+	sentences = LineSentence(data)
+	model = Word2Vec(sentences, size=100, window=5, min_count=5, workers=4)
+	model.wv.save_word2vec_format(filepath, binary=False)
+	print(filename + ' has been finished!')
 
 def batch_iter(data, batch_size, num_epochs, shuffle=True):
     """
